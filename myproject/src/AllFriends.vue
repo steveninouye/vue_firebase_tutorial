86a15229360af3a15ae971aca97cee489fc10063
Steven Inouye <steveninouye@msn.com>
2019-03-14 21:18:52 -0700

    Imported all friends and online friends components

---
+++ myproject/src/AllFriends.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="all-friends">
+    <h2>All Friends</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  anem: "AllFriends",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ myproject/src/OnlineFriends.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="online-friends">
+    <h2>Online Friends</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  anem: "OnlineFriends",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
