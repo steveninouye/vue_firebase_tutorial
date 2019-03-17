3c4853954b813530845c46fe6d0fa88908d133b5
Steven Inouye <steveninouye@msn.com>
2019-03-16 23:33:12 -0700

    Create all friend and online friends components

--- myproject/src/AllFriends.vue
+++ myproject/src/AllFriends.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="all-friends">
     <h2>All Friends</h2>
+    <div v-for="(friend, index) in friends" :key="index">
+      <span>{{friend.name}}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  anem: "AllFriends",
+  name: "AllFriends",
   data() {
-    return {};
+    return {
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Bowser", online: false }
+      ]
+    };
   }
 };
 </script>
--- myproject/src/OnlineFriends.vue
+++ myproject/src/OnlineFriends.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="online-friends">
     <h2>Online Friends</h2>
+    <div v-for="(friend, index) in friends" :key="index">
+      <span v-if="friend.online">{{friend.name}}</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  anem: "OnlineFriends",
+  name: "OnlineFriends",
   data() {
-    return {};
+    return {
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Bowser", online: false }
+      ]
+    };
   }
 };
 </script>
