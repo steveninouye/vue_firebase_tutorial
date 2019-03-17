57354c81632e662727eb92e35695c01b01901d70
Steven Inouye <steveninouye@msn.com>
2019-03-16 23:58:43 -0700

    Use created and before created lifecycle hook

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <h1>{{title}}</h1>
     <Navbar/>
+    <Blogs/>
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
+import Blogs from "./Blogs";
 
 export default {
   name: "app",
   components: {
-    Navbar
+    Navbar,
+    Blogs
   },
   data() {
     return {
---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogTitle: "Blogs"
+    };
+  },
+  beforeCreate() {
+    alert("before create hook");
+  },
+  created() {
+    alert("created hook");
+  },
+  beforeUpdate() {
+    
+  },
+};
+</script>
