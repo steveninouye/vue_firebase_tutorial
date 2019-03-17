c41155d850267f698a14d261f97b5c07877feca3
Steven Inouye <steveninouye@msn.com>
2019-03-17 15:48:54 -0700

    Create method for click event to go home

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="main-nav">
+  <nav class="main-nav">
     <ul>
       <li>
         <router-link to="/">Home</router-link>
@@ -8,14 +8,34 @@
         <router-link :to="{ name: 'About'}">About</router-link>
       </li>
     </ul>
-  </div>
+    <h2>User Profiles</h2>
+    <ul>
+      <li v-for="(id,index) in userIds" :key="index">
+        <router-link :to="{name: 'ViewProfile', params: {user_id: id}}">
+          <span>Profile {{ id }}</span>
+        </router-link>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <button @click="goHome">Redirect to Home</button>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      userIds: ["1", "2", "3", "4"]
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "Home" });
+    }
   }
 };
 </script>
