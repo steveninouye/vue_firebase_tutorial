29a7725fed19bb7bd1582cfc5c6e1a0611086baa
Steven Inouye <steveninouye@msn.com>
2019-03-17 15:34:50 -0700

    Created watch to watch for url changes and update component user id

--- routing/src/App.vue
+++ routing/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
+    <Navbar/>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Navbar
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
